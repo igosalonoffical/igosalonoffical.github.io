5437d12d4669ab9dcd8e1f19c80cdd4a419889e3
Sacc_Wu <Sacc19da@gmail.com>
2025-02-06 17:43:57 +0800

    變更mobile menu button style

--- scripts/common.js
+++ scripts/common.js
@@ -7,26 +7,32 @@ const mobileMenu = document.getElementById('mobileMenu');
 
 if (menuToggle) {
   // 菜單按鈕點擊事件
-  menuToggle.addEventListener('click', () => {
+  menuToggle.addEventListener('click', (event) => {
+    // 防止點擊菜單按鈕時，事件繼續傳遞到 document
+    event.stopPropagation(); 
+
+    // 切換菜單顯示與隱藏
     mobileMenu.classList.toggle('hidden');
+
+    // 切換三條線變成 X
+    menuToggle.classList.toggle('open');
   });
 }
 
 if (mobileMenu) {
-  // 點擊背景遮罩時關閉菜單
-  mobileMenu.addEventListener('click', (event) => {
-    if (event.target === mobileMenu) {
+  // 點擊菜單外部區域時隱藏菜單
+  document.body.addEventListener('click', (event) => {
+    if (!mobileMenu.classList.contains('hidden') && event.target !== menuToggle && !mobileMenu.contains(event.target)) {
       mobileMenu.classList.add('hidden'); // 隱藏菜單
-    } else if (event.target.tagName == 'A') {
-      mobileMenu.classList.add('hidden');
+      menuToggle.classList.remove('open'); // 還原三條線
     }
   });
 
-  document.body.addEventListener('click', (event) => {
-    if (mobileMenu.classList.contains('hidden') == false) {
-      if (event.target !== menuToggle && event.target !== mobileMenu) {
-        mobileMenu.classList.add('hidden'); // 隱藏菜單
-      }
+  // 點擊菜單中的超連結時隱藏菜單
+  mobileMenu.addEventListener('click', (event) => {
+    if (event.target.tagName === 'A') {
+      mobileMenu.classList.add('hidden'); // 隱藏菜單
+      menuToggle.classList.remove('open'); // 還原三條線
     }
   });
 }
